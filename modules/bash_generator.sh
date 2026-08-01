1d03eb6494c9711ccc972dcaa5c6329a09a9a179
HyperHackHub <rootaccessacademy@gmail.com>
2026-08-01 09:42:30 +0530

    bash_Generator

---
+++ modules/bash_generator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "================================================================================================"
+echo ""
+echo ""
+echo " 					Bash Generator 					      "
+echo ""
+echo ""
+echo "================================================================================================"
+
+echo ""
+echo ""
+
+echo " 1. Kali linux "
+echo " 2. Red Hat Linux"
+
+
+
+
+echo ""
+echo ""
+
+echo " 1. System Update & Upgrade "
+echo " 2. Change File permission "
+echo " 3. Move Files & Folders "
+echo " 4. Rename Files & Folders "
+echo " 5. Configure SSH "
+echo " 6. Install Package "
+echo " 7. More coming soon....."
+echo ""
+read -p " Choose one option to proceed : " ans
+echo ""
+
+#	if [ "$ans" = "1" ]; then 
+
+#		sudo 
+
